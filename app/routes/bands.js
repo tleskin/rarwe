857f5ac79626e6b5247ed038fa8b26f69365cc12
tleskin <tleskin@TLESKIN-MBP.local>
2017-04-06 12:18:24 -0400

    Finish chapter 5

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,22 +1,57 @@
 import Ember from 'ember';
 
 var Band = Ember.Object.extend({
-  name: ''
+  name: '',
+  songs: [],
+  slug: Ember.computed('name', function() {
+    return this.get('name').dasherize();
+  }),
+});
+
+var Song = Ember.Object.extend({
+    title: '',
+    band: '',
+    rating: 0
 });
 
 export default Ember.Route.extend({
 
   model: function(){
+    var blackDog = Song.create({ title: 'Black Dog',
+    band: 'Led Zeppelin', rating: 3
+    });
+
+    var yellowLedbetter = Song.create({
+      title: 'Yellow Ledbetter',
+      band: 'Pearl Jam',
+      rating: 4
+    });
+
+    var pretender = Song.create({
+      title: 'The Pretender',
+      band: 'Foo Fighters',
+      rating: 2
+    });
+
+    var daughter = Song.create({
+      title: 'Daughter',
+      band: 'Pearl Jam',
+      rating: 5
+    });
+
     var ledZeppelin = Band.create({
-      name: 'Led Zeppelin'
+      name: 'Led Zeppelin',
+      songs: [blackDog]
     });
 
     var pearlJam = Band.create({
-      name: 'Pearl Jam'
+      name: 'Pearl Jam',
+      songs: [yellowLedbetter, daughter]
     });
 
     var fooFighters = Band.create({
-      name: 'Foo Fighters'
+      name: 'Foo Fighters',
+      songs: [pretender]
     });
 
     return [ledZeppelin, pearlJam, fooFighters];
--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(params){
+    var bands =  this.modelFor('bands');
+
+    returns bands.findBy('slug', params.slug);
+  }
 });
